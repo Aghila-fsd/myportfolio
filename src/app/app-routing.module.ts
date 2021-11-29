b099c0c6c13ef20de1029eee704bc8d4f827f1a7
unknown <aghikrishna@gmail.com>
2021-11-29 20:43:36 +0530

    portfolio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { SamplesComponent } from './samples/samples.component';
 
-const routes: Routes = [];
+const routes: Routes = [  
+  {
+    path:'Home',
+    component:LandingpageComponent
+  },
+  {
+    path:'About',
+    component:AboutComponent
+  },
+  {
+    path:'Samples',
+    component:SamplesComponent
+  },
+  {
+    path:'Contact',
+    component:ContactComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AboutComponent } from './about/about.component';
+import { SamplesComponent } from './samples/samples.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+import { FooterComponent } from './footer/footer.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonyComponent } from './testimony/testimony.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AboutComponent,
+    SamplesComponent,
+    ContactComponent,
+    LandingpageComponent,
+    FooterComponent,
+    ServicesComponent,
+    TestimonyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    CarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
